47e7939191467a73529b54dbf06948e8a7a138d2
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-06-12 16:49:56 -0300

    Envio da Task02

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,12 +1,21 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Sara", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
