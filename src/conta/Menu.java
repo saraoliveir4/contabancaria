71c007a313e2137e24af8253018376b5c9b5b33a
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-06-14 15:57:05 -0300

    Envio Task03

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,6 +2,8 @@ package conta;
 
 import java.util.Scanner;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -16,6 +18,22 @@ public class Menu {
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Sara", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Sara", 100000.0f, 02);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
---
+++ src/conta/model/ContaPoupanca.java
@@ -0,0 +1,26 @@
+package conta.model;
+
+public class ContaPoupanca extends Conta {
+
+	private int aniversario;
+
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+		super(numero, agencia, tipo, titular, saldo);
+		// TODO Auto-generated constructor stub
+		this.aniversario = aniversario;
+	}
+
+	public int getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	@Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("\nAniversário da Conta: " + this.aniversario);
+	}
+}
